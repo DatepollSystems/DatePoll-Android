42ca28af9eef1d3878da9634ce98ffaf901ddb98
kaulex <2817137-kaulex@users.noreply.gitlab.com>
2020-02-18 20:01:33 +0100

    #4 added step 2

--- app/src/main/java/com/bke/datepoll/ui/settings/SettingsChangePasswordFragment.kt
+++ app/src/main/java/com/bke/datepoll/ui/settings/SettingsChangePasswordFragment.kt
@@ -1,23 +1,20 @@
 package com.bke.datepoll.ui.settings
 
-
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
 import com.bke.datepoll.R
 import kotlinx.android.synthetic.main.fragment_settings_change_password.*
 import kotlinx.android.synthetic.main.fragment_settings_change_password.view.*
 
 class SettingsChangePasswordFragment : Fragment() {
 
-    enum class Step { ONE, TWO, THREE}
+    enum class Step { ONE, TWO, THREE }
 
     var currentStep: Step = Step.ONE
 
@@ -25,14 +22,13 @@ class SettingsChangePasswordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+
         val view = inflater.inflate(R.layout.fragment_settings_change_password, container, false)
 
         view.btnConfirmOldPasswd.setOnClickListener {
             changeCurrentStep(true)
         }
-
-
 
         return view
     }
@@ -42,7 +38,7 @@ class SettingsChangePasswordFragment : Fragment() {
             .onBackPressedDispatcher
             .addCallback(this, object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
-                    when(currentStep){
+                    when (currentStep) {
                         Step.ONE -> findNavController().popBackStack()
                         Step.TWO -> changeCurrentStep(false)
                         Step.THREE -> changeCurrentStep(false)
@@ -56,23 +52,28 @@ class SettingsChangePasswordFragment : Fragment() {
     /**
      * @param direction true -> next step; false -> one step back
      */
-    private fun changeCurrentStep(direction: Boolean){
-        when(currentStep){
+    private fun changeCurrentStep(direction: Boolean) {
+        when (currentStep) {
             Step.ONE -> {
-                if(direction){
+                if (direction) {
 
                     //TODO check if old password is valid
 
                     currentStep = Step.TWO
                     tiOldPassword.visibility = View.GONE
                     btnConfirmOldPasswd.visibility = View.GONE
+
+                    tiNewPassword.visibility = View.VISIBLE
+                    tiConfirmNewPassword.visibility = View.VISIBLE
+                    btnConfirmNewPassword.visibility = View.VISIBLE
                 } else {
                     Log.i(tag, "First step, can't go back anymore")
                 }
             }
             Step.TWO -> {
-                if(direction){
+                if (direction) {
                     currentStep = Step.THREE
+
 
                 } else {
                     currentStep = Step.ONE
