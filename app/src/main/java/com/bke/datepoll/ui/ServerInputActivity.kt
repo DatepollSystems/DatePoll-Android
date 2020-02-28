98b2c2b969d90bf01d628b9112e9ca9dc9f5421b
kaulex <2817137-kaulex@users.noreply.gitlab.com>
2020-02-28 18:20:45 +0100

    fix

--- app/src/main/java/com/bke/datepoll/ui/ServerInputActivity.kt
+++ app/src/main/java/com/bke/datepoll/ui/ServerInputActivity.kt
@@ -42,8 +42,6 @@ class ServerInputActivity : AppCompatActivity() {
             advancedServerSettings.animateVisibility(true)
         }
 
-        supportFragmentManager
-
         etServerAddress.addTextChangedListener(object : TextWatcher {
 
             override fun afterTextChanged(s: Editable) {
